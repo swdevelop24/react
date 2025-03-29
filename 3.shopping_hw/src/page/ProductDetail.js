a3091d445e8ec6b9b45a3398a1744b9bddeb52e1
swdevelop24 <swdevelop24@gmail.com>
2025-03-28 23:21:04 -0500

    fix: server address

--- 3.shopping_hw/src/page/ProductDetail.js
+++ 3.shopping_hw/src/page/ProductDetail.js
@@ -18,7 +18,7 @@ const ProductDetail = () => {
 
 
   const getProductDetail = async () => {
-    let url = `http://localhost:5000/products/${id}`
+    let url = `https://my-json-server.typicode.com/swdevelop24/react/products/${id}`
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
