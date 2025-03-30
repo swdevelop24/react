64327ac08fbc780281ddc0b1ece3fc96d6385feb
swdevelop24 <swdevelop24@gmail.com>
2025-03-30 15:32:19 -0500

    fix: http to https

--- 2_weather-app_hw/src/App.js
+++ 2_weather-app_hw/src/App.js
@@ -65,7 +65,7 @@ function App() {
   const getWeatherByCity = async () => {
 
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       setLoading(true);
       let response = await fetch(url)
       let data = await response.json()
