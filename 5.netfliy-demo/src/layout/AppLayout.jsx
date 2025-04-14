07129255ca70420fbee3d4a3116ac49a4ba3f9ee
swdevelop24 <swdevelop24@gmail.com>
2025-04-14 09:13:52 -0500

    feat: before filter and sort

--- 5.netfliy-demo/src/layout/AppLayout.jsx
+++ 5.netfliy-demo/src/layout/AppLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
+import { Modal, Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
@@ -9,20 +9,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./AppLayout.style.css";
 import { Outlet, Link, useSearchParams, useNavigate } from "react-router-dom";
 
-
 const AppLayout = () => {
+  // 입력값 받아오기
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
-  // 입력값 받아오기 
-  const [keyword, setKeyword] = useState(""); 
-  const navigate = useNavigate()
-  const searchByKeyword=(event)=>{
-    // form 인경우 리프레스 방지해줘야함 
-    event.preventDefault() 
-    // url 바꿔주기 
-    navigate(`/movies?q=${keyword}`)
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  const searchByKeyword = (event) => {
+    event.preventDefault();
+    if (!keyword.trim()) {
+      handleShow(); // 모달 띄우기
+      return;
+    }
+    navigate(`/movies?q=${keyword}`);
     setKeyword("");
-    
-  }
+  };
+
   return (
     <div className="nav-container">
       <Navbar expand="lg" variant="dark" style={{ background: "black" }}>
@@ -39,10 +44,7 @@ const AppLayout = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              navbarScroll
-            >
+            <Nav className="me-auto my-2 my-lg-0" navbarScroll>
               <Nav.Link as={Link} to="/" className="text-white">
                 Home
               </Nav.Link>
@@ -50,7 +52,10 @@ const AppLayout = () => {
                 Movies
               </Nav.Link>
             </Nav>
-            <Form className="d-flex nav-custom-search-container" onSubmit={searchByKeyword}>
+            <Form
+              className="d-flex nav-custom-search-container"
+              onSubmit={searchByKeyword}
+            >
               <Form.Control
                 className="nav-custom-search-box"
                 type="search"
@@ -59,12 +64,26 @@ const AppLayout = () => {
                 value={keyword}
                 onChange={(event) => setKeyword(event.target.value)}
               />
-              <Button variant="danger" type="submit">Search</Button>
+              <Button variant="danger" type="submit">
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
+        <Modal show={showModal} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>⚠️ 입력 오류</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>검색어를 입력해주세요!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={handleClose}>
+              닫기
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        ;
       </Navbar>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 };
--- 5.netfliy-demo/src/pages/Movies/MoviePage.jsx
+++ 5.netfliy-demo/src/pages/Movies/MoviePage.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
+import ReactPaginate from "react-paginate";
+import { useEffect } from "react";
+
 import { Spinner, Alert, Container, Col, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieCard from "../../common/MovieCard/MovieCard";
-import "./MoviePage.style.css"
+import "./MoviePage.style.css";
 
 //경로 2가지
 //navbar 에서 무비즈 탭을 클릭해서 온경우 => popularMovie 보여주기
 //keyword를 입력해서 온경우 => keyword와 관련된 영화들을 보여줌
+
+//페이지네이션 설치
+//page state 만들기
+//페이지네이션 클릭할때마다 page 바꿔주기
+//page 값이 바뀔때마다 useSearchMovie에 page까지 넣어서 fetch
+
 const MoviePage = () => {
   const [query, setQuery] = useSearchParams();
   // url에 q 지우고 popular movie 오는지도 테스트해보기
+
+  const [page, setPage] = useState(1);
   const keyword = query.get("q");
 
   //{keyword} object로 받는거 hook에서도 일치시키기! 헷갈리기 쉬움
-  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+
+  useEffect(() => {
+    setPage(1);
+  }, [keyword]);
 
   if (isLoading) {
     return (
@@ -34,41 +52,110 @@ const MoviePage = () => {
   }
 
   console.log("ddd", data);
+
+  const handlePageClick = ({ selected }) => {
+    console.log("page", page);
+    setPage(selected + 1);
+  };
+
+    // 검색어는 있는데 결과가 없는 경우
+    const noResult = keyword && data?.results?.length === 0;
+
   return (
-    // <Container >
-    //   <Row   >
-    //     <Col lg={5} xs={12}>
-    //       필터
+
+    // <Container fluid className="px-4 px-md-5">
+    //   <Row className="gx-4 gy-4">
+    //     <Col lg={4} xs={12}>
+    //       <div className="text-white">필터</div>
     //     </Col>
-    //     <Col lg={7} xs={12}>
-    //       <Row  className="gy-4">
+
+    //     <Col lg={8} xs={12}>
+    //       <Row className="gx-4 gy-4">
     //         {data?.results.map((movie, index) => (
-    //           <Col key={index} lg={4} xs={12}>
+    //           <Col key={index} lg={4} md={6} sm={12}>
     //             <MovieCard movie={movie} />
     //           </Col>
     //         ))}
     //       </Row>
+    //       <ReactPaginate
+    //         nextLabel="next >"
+    //         onPageChange={handlePageClick}
+    //         pageRangeDisplayed={3}
+    //         marginPagesDisplayed={2}
+    //         pageCount={data?.total_pages} //전체페이지가 몇개인지 알려줌
+    //         previousLabel="< previous"
+    //         pageClassName="page-item"
+    //         pageLinkClassName="page-link"
+    //         previousClassName="page-item"
+    //         previousLinkClassName="page-link"
+    //         nextClassName="page-item"
+    //         nextLinkClassName="page-link"
+    //         breakLabel="..."
+    //         breakClassName="page-item"
+    //         breakLinkClassName="page-link"
+    //         containerClassName="custom-pagination"
+    //         activeClassName="active"
+    //         renderOnZeroPageCount={null}
+    //         forcePage={data?.page ? page - 1 : 0}  // fallback to 0
+    //       />
     //     </Col>
     //   </Row>
     // </Container>
     <Container fluid className="px-4 px-md-5">
-  <Row className="gx-4 gy-4">
-    <Col lg={4} xs={12}>
-      <div className="text-white">필터</div>
-    </Col>
+    <Row className="gx-4 gy-4">
+      <Col lg={4} xs={12}>
+        <div className="text-white">필터</div>
+      </Col>
 
-    <Col lg={8} xs={12}>
-      <Row className="gx-4 gy-4">
-        {data?.results.map((movie, index) => (
-          <Col key={index} lg={4} md={6} sm={12}>
-            <MovieCard movie={movie} />
-          </Col>
-        ))}
-      </Row>
-    </Col>
-  </Row>
-</Container>
+      <Col lg={8} xs={12}>
+        {noResult ? (
+          <div className="text-white mt-5">
+            <div className="fs-4 mb-3 ">검색 결과가 없습니다!!</div>
+            <button
+              className="btn btn-outline-light"
+              onClick={() => window.history.back()}
+            >
+              ← Go Back
+            </button>
+          </div>
+        ) : (
+          <>
+            <Row className="gx-4 gy-4">
+              {data?.results.map((movie, index) => (
+                <Col key={index} lg={4} md={6} sm={12}>
+                  <MovieCard movie={movie} />
+                </Col>
+              ))}
+            </Row>
 
+            {data?.total_pages > 0 && (
+              <ReactPaginate
+                nextLabel="next >"
+                previousLabel="< previous"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={data?.total_pages}
+                containerClassName="custom-pagination"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+                forcePage={data?.page ? page - 1 : 0}
+              />
+            )}
+          </>
+        )}
+      </Col>
+    </Row>
+  </Container>
   );
 };
 
